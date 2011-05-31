ae5abaa0654fcd6aaa96fe67a999db8a15d9f5fb
Nathan Bertram <nbertram@gmail.com>
2011-05-31 17:15:07 -0500

    Base install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JustForScience::Application.config.secret_token = 'e9116b053aa1f59d230f739d9259335c3ffb28619d369366468e124d1123192d21847060ad6759c52c0883af8fce854d79bea5b18cd20bb049ba2508d8f2e319'
